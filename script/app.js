bbd5972395cc0630190a247cd9c0f729265e7855
h fam <h fam>
2017-06-09 16:31:47 -0600

    twitter portion nixed not working or looking how I want

--- script/app.js
+++ script/app.js
@@ -1,28 +1,33 @@
+
+
 // This array will generate the "search" categories on the Giphy Site
 var choices = [{movie: "1. Blazing Saddles", data: "Blazing+Saddles" }, {movie: "2. Airplane" , data: "Airplane"}, {movie: "3. Monty Python & the Holy Grail", data: "Monty+Python+and+the+Holy+Grail"}, {movie: "4. Animal House", data: "Animal+House"},{movie: "5. Young Frankenstein", data: "Young+Frankenstein"}, {movie: "6. Caddyshack", data: "Caddyshack"}, {movie: "7. This is Spinal Tap", data: "This+is+Spinal+Tap"}, {movie: "8. The Life of Brian", data: "The+Life+of+Brian"}, {movie: "9. Dumb & Dumber", data: "Dumb+Dumber"}, {movie: "10. The Naked Gun", data: "The+Naked+Gun"}, {movie: "11. The Jerk", data: "The+Jerk"}, {movie: "12. The Big Lebowski", data: "The+Big+Lebowski"}, {movie: "13. There's Something About Mary", data: "Something+About+Mary"}, {movie: "14. Borat", data: "Borat"}, {movie: "15. A Fish Called Wanda", data: "Fish+Called+Wanda"}, {movie: "16. The 40-Year-Old Virgin", data: "40+year+old+virgin"}, {movie: "17. Step Brothers", data: "Step+Brothers"}, {movie: "18. Anchorman", data: "Anchorman"}, {movie: "19. Office Space", data: "Office+Space"}, {movie: "20. Planes Trains and Automobiles", data: "Planes+Trains+and+automobiles+movie"}, {movie: "21. Raising Arizona", data: "Raising+Arizona"}, {movie: "22. Dr. Strangelove", data: "Dr+Strangelove"}, {movie: "23. Blues Brothers", data: "Blues+Brothers"}, {movie: "24. Tommy Boy", data: "Tommy+Boy"}, {movie: "25. Superbad", data: "Superbad"} ];
+// Definition of Variables
 var userInput;
 var userClick;
 var url;
 var switchVal;
 var switchBack;
 var clicked = false;
-
+// This function controls whether still image or animated gif
 function whatImage (x) {
-				if (clicked) {
-				$(x).attr("src", $(x).attr("data-switch"));
-				
-			}
-				else {
-					$(x).attr("src", $(x).attr("data-switch-back"));
-				}
+	if (clicked) {
+		$(x).attr("src", $(x).attr("data-switch"));
+		
+	}
+	else {
+		$(x).attr("src", $(x).attr("data-switch-back"));
+	}
 };
+// This function displays buttons
 function displayButtons () {
 	for (var i = 0; i < choices.length; i++){
 		$(".buttonArea").append("<button class='btn btn-danger btn-md clickSearch' data-search=" + choices[i].data + ">" + choices[i].movie + "</button>");
 	}
 };
+// This button actually goes to Giphy and granbs the images and displays them
 function searchForImages () {
-			$(".clickSearch").on("click", function () {
+	$(".clickSearch").on("click", function () {
 		$(".imageArea").empty();
 		$(".imageArea").append("<h1 class='center-block'>" + $(this).html() + "</h1>");
 		userClick = $(this).attr("data-search");
@@ -32,25 +37,23 @@ function searchForImages () {
 			url: url,
 			method: "GET"
 		}).done(function(response) {
-			for (var i = 0; i < 9; i++){
-				$(".imageArea").append("<div class='col-lg-4 img-responsive clickOn'><span>Rating: " + response.data[i].rating.toUpperCase() + "</span><br/><img class='grabMe' data-switch='" + response.data[i].images.fixed_width.url + "' data-switch-back='" + response.data[i].images.fixed_width_still.url + "' src='" + response.data[i].images.fixed_width_still.url + "'>");
-
-				
+			for (var i = 0; i < 10; i++){
+				$(".imageArea").append("<div class='col-lg-3 img-responsive clickOn'><span>Rating: " + response.data[i].rating.toUpperCase() + "</span><br/><img class='grabMe' data-switch='" + response.data[i].images.fixed_width.url + "' data-switch-back='" + response.data[i].images.fixed_width_still.url + "' src='" + response.data[i].images.fixed_width_still.url + "'>");	
 			}
 			$(".grabMe").click(function () {
 				if (clicked) {
-				clicked = false;	
-				whatImage(this);
-			}
-			else {
-				clicked = true;
-				whatImage(this);
-			}
-	
+					clicked = false;	
+					whatImage(this);
+				}
+				else {
+					clicked = true;
+					whatImage(this);
+				}
+				
 			});
-		
+			
+		});
 	});
-});
 
 }
 
@@ -59,7 +62,7 @@ function searchForImages () {
 $(document).ready(function(){
 	$(".buttonArea").append("<h2>Top 25 Comedy Movies according to Rolling Stone</h2>");
 	displayButtons();
-	$(".imageArea").append("<div class='row'><h1 class='center-block'>Top Trending Photos</h1><p class='center-block'>Click a comedy from above to see more GIFS!</p></div>");
+	$(".imageArea").append("<div class='row'><h1 class='lookBetter'>Top Trending Photos</h1><p class='lookBetter'>Click a comedy from above to see more GIFS!</p></div>");
 	$.ajax({
 		url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
 		method: "GET"
@@ -67,11 +70,11 @@ $(document).ready(function(){
 		for (var i = 0; i < 9; i++){
 			console.log(response.data[i].images.fixed_width.url);
 
-			$(".imageArea").append("<div class='col-lg-4'><span>Rating: " + response.data[i].rating.toUpperCase() + "</span><br/><img src='" + response.data[i].images.fixed_width.url + "'>");
+			$(".imageArea").append("<div class='col-lg-3'><span>Rating: " + response.data[i].rating.toUpperCase() + "</span><br/><img class='img-responsive' src='" + response.data[i].images.fixed_width.url + "'>");
 		};
 	});
-		searchForImages();
-		$(".buttonToAdd").on("click", function (event) {
+	searchForImages();
+	$(".buttonToAdd").on("click", function (event) {
 		event.preventDefault();
 		var tempVar = $("#addToArray").val()
 		choices.push({movie: tempVar, data: tempVar.replace(" ", "+")});
@@ -81,3 +84,5 @@ $(document).ready(function(){
 		$("#addToArray").val("");
 	});
 });
+
+
